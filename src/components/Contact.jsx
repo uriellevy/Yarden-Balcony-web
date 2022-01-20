969992c1068fc1bbc804f5890c30f2cc266c1442
uriel levy <uriellevy4892@gmail.com>
2022-01-20 10:50:39 +0200

    email activation

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,21 +1,42 @@
 import React from "react";
 import "./styles/Contact.css";
 import { AiOutlineContacts } from "react-icons/ai";
-
+import emailjs from "emailjs-com";
 
 const Contact = () => {
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm(
+        "service_n1v58o9",
+        "template_aitd6fc",
+        e.target,
+        "user_crcSLVsR3W2BhTJ0IJTYF"
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+    e.target.reset();
+  };
   return (
     <div className="contact" id="contact">
       <div className="contact-header">
         <AiOutlineContacts className="header-icon" />
         <h1>להזמנות ויצירת קשר</h1>
       </div>
-      <form className="contact-form">
+      <form className="contact-form" onSubmit={sendEmail}>
         <input
           type="text"
           placeholder="שם מלא*"
           className="form-control"
           aria-label="Name"
+          name="name"
           dir="rtl"
         />
         <input
@@ -23,6 +44,7 @@ const Contact = () => {
           placeholder="מס' טלפון*"
           className="form-control"
           aria-label="Phone Number"
+          name="phone"
           dir="rtl"
         />
         <input
@@ -30,6 +52,7 @@ const Contact = () => {
           aria-label="Message"
           placeholder="תוכן פניה*"
           className="form-control form-message"
+          name="message"
           dir="rtl"
           required
         />
